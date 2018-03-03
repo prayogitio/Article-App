5de8002930b61c89ba25c7ddd787310d5bcbbb3f
prayogitio <tio.qynxx@gmail.com>
2018-03-03 17:08:43 +0700

    update html and setting

--- yuhuuarticle/settings.py
+++ yuhuuarticle/settings.py
@@ -124,11 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join((BASE_DIR), 'assets')
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'assets'),
-)
 
 MEDIA_URL = "/media/"
 
